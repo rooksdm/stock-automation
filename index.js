4d388099843b002e54035c33bc443174020aca2c
Levbert Leger <levbert@rooksdm.com>
2021-05-29 12:28:21 -0500

    clean up and symbol args

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ const excelToJson = require('convert-excel-to-json');
 var json2xls = require('json2xls');
 const fs = require('fs');
 const Excel = require('exceljs');
-const { isRegExp } = require('util');
 const args = process.argv.slice(2)
-console.log(args)
 const path = args[0]
 const parentPath = path.split('\\').reverse().splice(2).reverse().join('\\')
-console.log(path)
-console.log(parentPath)
-const symbolsString = 'AP,CTVA,DD,DOW,IFF,KL,NTR,NUE,PPG,CHTR,CMCSA,T,TMUS,VZ,AMZN,BABA,BOOT,BWA,DBI,DIS,FOSL,GM,HD,LKQ,LOW,MCD,NFLX,PLCE,SBUX,TGT,ADM,CL,COST,GHC,GIS,K,KO,MO,NSRGY,PEP,PG,PM,TRMB,UL,WBA,WMT,BP,COP,CVX,ENB.TO,KMI,MPC,RDSB,VLO,XOM,AXP,BNS.TO,BRKB,HSBC,ICE,JPM,MA,MCO,PGR,PYPL,RY.TO,SCHW,SPGI,V,WFC,ABBV,ABT,AMGN,BMY,CVS,DHR,ISRG,JNJ,LLY,MDT,MRK,NVS,PFE,TMO,UNH,BA,CARR,CSX,DE,EMR,FDX,GD,ITW,LUV,MMM,MORN,NSC,OTIS,RTX,UAL,UNP,UPS,WEC,AAPL,ACN,ADBE,AKAM,AMAT,AMD,ANSS,ANTM,CSCO,FB,FTV,GLW,GOOG,HOLX,HPQ,IBM,INTC,MSFT,ORCL,QCOM,TXN,ZS,MLR,CNP,D,DNP,DUK,ED,EXC,LNT,NEE,SO,SRE'
+const symbolsString = args[1]
 const symbolsArray = symbolsString.toUpperCase().split(',')
 const outArray = []
 const dowTheory = excelToJson({
@@ -63,7 +59,7 @@ symbolsArray.forEach(symbol => {
 });
 
 var xls = json2xls(outArray);
-//fs.writeFileSync(path + '\\stocks.xlsx', xls, 'binary');
+fs.writeFileSync(path + '\\combined.xlsx', xls, 'binary');
 const alphabet = ' abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
 
 async function updateExcel(){
@@ -75,13 +71,10 @@ async function updateExcel(){
     worksheet.eachRow(function(row, rowNumber) {
         newValues = {}
         if(rowNumber === 1){
-            //console.log('Row ' + rowNumber + ' = ' + JSON.stringify(row.values));
             for (let i = 1; i < row.values.length; i++) {
                 const element = row.values[i];
                 colMap[element] = {column: alphabet[i], index: i}
             }
-            //console.log(JSON.stringify(colMap))
-            // console.log(JSON.stringify(outArray))
         }else {
             const ticker = row.values[colMap['Ticker'].index]
             newValues = outArray.find(obj => obj['Ticker'] === ticker)
@@ -89,7 +82,7 @@ async function updateExcel(){
                 console.log(`NewValues ${JSON.stringify(newValues)} ${rowNumber} issue ${row.values[colMap['Ticker'].index]}`)
             } else {
                 for (let key in newValues){
-                   // worksheet.getCell(`${colMap[key].column}${rowNumber}`).value = parseInt(newValues[key]) || newValues[key]
+                   worksheet.getCell(`${colMap[key].column}${rowNumber}`).value = parseInt(newValues[key]) || newValues[key]
                 }
             }
         }
@@ -98,5 +91,3 @@ async function updateExcel(){
 
 }
 updateExcel()
-
-// console.log(JSON.stringify(outArray.find(obj => obj['Ticker'] === 'ZS')))
